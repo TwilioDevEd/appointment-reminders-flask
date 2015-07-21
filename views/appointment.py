bb9589297facec7b36606d75c41e1dea84558dab
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-07-21 17:56:22 -0500

    Add first template

--- views/appointment.py
+++ views/appointment.py
@@ -1,4 +1,5 @@
 from flask.views import MethodView
+from flask import render_template
 from models.appointment import Appointment
 import datetime
 
@@ -10,4 +11,4 @@ class AppointmentResource(MethodView):
         appt = Appointment('A name', '+54853435', 120, datetime.datetime.now(), 'Americas/Guayaquil')
         self.db.session.add(appt)
         self.db.session.commit()
-        return "something"
+        return render_template('base.j2.html', name=None)
